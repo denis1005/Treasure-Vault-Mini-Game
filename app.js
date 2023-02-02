163184df33f6352f38c1dac54648d172f255a3f9
Denislav <denis1005@users.noreply.github.com>
2023-02-02 21:35:15 +0200

    Generates a random secret combination

--- app.js
+++ app.js
@@ -7,5 +7,23 @@
   })
 
   document.body.appendChild(app.view)
+
+let secret = generateSecret();
+console.log("The secret combination is:", secret);
+
+
+
+// Generates a random secret combination
+function generateSecret() {
+   let secret = [];
+   for (let i = 0; i < 3; i++) {
+     let number = Math.floor(Math.random() * 9) + 1;
+     let direction = Math.random() < 0.5 ? "clockwise" : "counterclockwise";
+     secret.push({ number, direction });
+   }
+   return secret;
+ }
+
+ 
   
   
