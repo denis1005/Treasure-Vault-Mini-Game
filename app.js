5cd5b38379c619168e0a7e62cda29cbad37d2705
Denislav <denis1005@users.noreply.github.com>
2023-02-02 21:39:57 +0200

    Adding and positioning door close

--- app.js
+++ app.js
@@ -21,7 +21,20 @@ const backgroundTexture=PIXI.Texture.from('./images/bg.png')
   // Center the background on the screen
   backgroundSprite.x = (app.view.width - backgroundSprite.width) / 2;
   backgroundSprite.y = (app.view.height - backgroundSprite.height) / 2;
+  
+  
+  //Creating Door Open texture
+  const doorOpenTexture=PIXI.Texture.from('./images/doorOpen.png')
+  //Creating Adding door
+  const doorCloseTexture=PIXI.Texture.from('./images/door.png')
 
+  let doorSprite=new PIXI.Sprite(doorCloseTexture);
+  app.stage.addChild(doorSprite)
+  
+  doorSprite.width = backgroundSprite.width/3;
+  doorSprite.height = backgroundSprite.height/1.5;
+  doorSprite.x = (app.view.width - doorSprite.width) / 2;
+  doorSprite.y = (app.view.height - doorSprite.height) / 2;
 
 
 // Generates a random secret combination
