b2abaec9de40fad393b1f8a2bc3f293b7b882969
Denislav <denis1005@users.noreply.github.com>
2023-02-02 21:38:00 +0200

    Adding and cantering background image

--- app.js
+++ app.js
@@ -11,6 +11,17 @@
 let secret = generateSecret();
 console.log("The secret combination is:", secret);
 
+const backgroundTexture=PIXI.Texture.from('./images/bg.png')
+  const backgroundSprite=new PIXI.Sprite(backgroundTexture);
+  app.stage.addChild(backgroundSprite)
+ 
+  backgroundSprite.width = app.view.width;
+  backgroundSprite.height = app.view.height;
+
+  // Center the background on the screen
+  backgroundSprite.x = (app.view.width - backgroundSprite.width) / 2;
+  backgroundSprite.y = (app.view.height - backgroundSprite.height) / 2;
+
 
 
 // Generates a random secret combination
@@ -24,6 +35,6 @@ function generateSecret() {
    return secret;
  }
 
- 
+
   
   
